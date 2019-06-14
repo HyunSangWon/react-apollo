12fc2101c0c129c1f731da385ab77307ace3df59
Sangwon Hyun <hyunsangwon93@gmail.com>
2019-06-14 15:57:41 +0900

    react apollo 작업중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Query} from "react-apollo";
+import client from "./apolloClient";
+import {HashRouter as Router,Route} from "react-router-dom";
+import {ApolloProvider} from "react-apollo";
+import Home from "./Home";
+import Detail from "./Detail";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact={true} path={"/"} component={Home}/>
+        <Route exact={true} path={"/details/:movieId"} component={Detail}/>
+      </Router>
+    </ApolloProvider>
   );
 }
 
