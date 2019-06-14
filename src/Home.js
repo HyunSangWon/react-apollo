17902d213a9595682b2375c3b2e918df74ea6cf0
Sangwon Hyun <hyunsangwon93@gmail.com>
2019-06-14 16:22:26 +0900

    react apollo 작업중

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import {Query} from "react-apollo";
+import {HOME_PAGE} from "./queries"
 
-const Home = () => <div>Home</div>
+const Home = () => <Query query={HOME_PAGE}>{({ loading, data, error})=>{
+    if(loading) return <span>loading</span>
+    if(error) return <span>something happened</span>
+    if(data){
+        console.log(data);
+        return <span>yay</span>
+    }
+}}</Query>
 
 export default Home;
